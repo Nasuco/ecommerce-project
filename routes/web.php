cf99d4efe8fe52857e71102426e9ee71b46e249e
Nasuco <2011501026@student.unisayogya.ac.id>
2024-12-04 20:37:29 +0700

    add checkout page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
@@ -23,3 +24,4 @@ Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/products/{product}', ProductDetailPage::class);
 Route::get('/cart', CartPage::class);
+Route::get('/checkout', CheckoutPage::class);
