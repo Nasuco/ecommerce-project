bdfc19b9ccb719442bd6140c7a0f7b813c081274
Nasuco <2011501026@student.unisayogya.ac.id>
2024-12-04 20:34:28 +0700

    add product detail page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
 Route::get('/cart', CartPage::class);
