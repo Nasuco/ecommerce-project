e336cfd7f83987d81046084bd095b541dd109be1
Nasuco <2011501026@student.unisayogya.ac.id>
2024-12-04 20:26:36 +0700

    add categories page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
