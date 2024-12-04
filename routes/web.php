2a46b2a6de3abe71297d6f1f03a8dd5608596b46
Nasuco <2011501026@student.unisayogya.ac.id>
2024-12-04 20:53:22 +0700

    add cancel page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\Auth\ForgotPasswordPage;
 use App\Livewire\Auth\LoginPage;
 use App\Livewire\Auth\RegisterPage;
 use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\CheckoutPage;
@@ -39,3 +40,5 @@ Route::get('/login', LoginPage::class);
 Route::get('/register', RegisterPage::class);
 Route::get('/forgot', ForgotPasswordPage::class);
 Route::get('/reset', ResetPasswordPage::class);
+
+Route::get('/cancel', CancelPage::class);
