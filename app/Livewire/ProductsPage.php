f7ea6023aa5f70618a99825355c6f32f184a80c0
Nasuco <2011501026@student.unisayogya.ac.id>
2024-12-05 10:15:22 +0700

    show product detail from data at product detail page

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Products - Page Title')]
 class ProductsPage extends Component
 {
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6),
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
+        ]);
     }
 }
